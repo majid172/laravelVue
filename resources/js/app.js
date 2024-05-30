c6ade622df695d6f4f00bb63236296fc1b716931
Majid Khan <luthfullahhilmajid172@gmail.com>
2024-05-31 00:47:37 +0600

    laravel installation with vue js

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+// import { createApp } from 'vue';
+// import App from './components/app.vue';
+// const app=createApp(App);
+// app.mount('#app');
+
+import { createApp } from 'vue';
+import App from './components/app.vue';
+
+createApp(App).mount('#app');
---
+++ resources/js/components/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>Hello</h1>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    name: 'App'
+  }
+  </script>
+
+  <style scoped>
+  /* Add any component-specific styles here */
+  </style>
